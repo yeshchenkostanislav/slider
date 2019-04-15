01bf3bcd0d35117119716d1e223fa844c0fc05e5
Stanislav Yeshchenko <yeshchenkostanislav@gmail.com>
2019-04-16 00:37:53 +0200

    Перезалил работу

--- js/parts/other.js
+++ js/parts/other.js
@@ -1,9 +1,50 @@
 function other() {
 
-  let slider = document.querySelector('.slider'),
+  // высота слайдов которая высчитывается исходя из высоты экрана
+  let sliderItem = document.querySelector('.slider__item'),
     heightWindow = document.documentElement.clientHeight;
 
-  slider.style.height = heightWindow + 'px';
+  sliderItem.style.height = heightWindow + 'px';
+
+  // создаем бызу данных зарегистрированных email
+
+  let mailbox = document.querySelector('.input'),
+    button = document.querySelector('.header-input-btn'),
+    text = document.querySelector('.subinput__domain'),
+    base = [];
+
+
+
+
+  button.addEventListener('click', () => {
+
+    if (mailbox.value != '' && mailbox.value != null) {
+
+      let a;
+      for (var i = 0; i < base.length; i++) {
+        if (base[i] === mailbox.value) {
+          a = true;
+          text.style.display = 'none';
+        }
+      }
+
+      if (a) {
+        alert('Домен с таким именем уже существует');
+        mailbox.value = '';
+      } else {
+        base.push(mailbox.value);
+        text.innerHTML = 'Домен ' + mailbox.value + ' — свободен.';
+        text.style.display = 'block';
+        mailbox.value = '';
+        console.log(base);
+      }
+    } else {
+      alert("Введи почту");
+    };
+
+
+  })
+
 }
 
 module.exports = other;
--- js/parts/slider.js
+++ js/parts/slider.js
@@ -1,54 +1,68 @@
 function slider() {
-  /* 
-    let slideIndex = 1,
-      slides = document.querySelectorAll('.slider-item'),
-      prev = document.querySelector('.prev'),
-      next = document.querySelector('.next'),
-      dotsWrap = document.querySelector('.slider-dots'),
-      dots = document.querySelectorAll('.dot');
 
-    showSlides(slideIndex);
+  let slideIndex = 1,
+    slides = document.querySelectorAll('.slider__item'),
+    bonusesWrap = document.querySelectorAll('.bonuses-wrap'),
+    ollTabs = document.querySelectorAll('.bonuses-wrap-tabs__item'),
+    dotsWrap = document.querySelector('.slider-dots'),
+    dots = document.querySelectorAll('.dot');
 
-    function showSlides(n) {
+  showSlides(slideIndex);
 
-      if (n > slides.length) {
-        slideIndex = 1;
-      }
-      if (n < 1) {
-        slideIndex = slides.length;
-      }
+  function showSlides(n) {
 
-      slides.forEach((item) => item.style.display = "none");
-
-      dots.forEach((item) => item.classList.remove('dot-active'));
-
-      slides[slideIndex - 1].style.display = 'block';
-      dots[slideIndex - 1].classList.add('dot-active');
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
     }
 
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
+    slides.forEach((item) => item.style.display = "none");
 
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
+    dots.forEach((item) => item.classList.remove('dot-active'));
 
-    prev.addEventListener('click', () => {
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('dot-active');
+  }
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  //почле прокрутки колечика убираю расстояние которое добавлялось при появлении таба
+  function hideMargin() {
+    bonusesWrap.forEach((item) => {
+      item.style.marginBottom = '50px';
+    })
+  }
+
+  // отслеживаю прокрутку колесика мышки и переключаю слайды
+  document.addEventListener('wheel', (e) => {
+    if (e.wheelDelta == 120) {
       plusSlides(-1);
-    });
-
-    next.addEventListener('click', () => {
+      ollTabs.forEach((item) => item.classList.add('hide'));
+      hideMargin();
+    } else {
       plusSlides(1);
-    });
+      ollTabs.forEach((item) => item.classList.add('hide'));
+      hideMargin();
+    }
+  });
 
-    dotsWrap.addEventListener('click', function (event) {
-      for (let i = 0; i < dots.length + 1; i++) {
-        if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
-          currentSlide(i);
-        }
+
+
+  dotsWrap.addEventListener('click', function (event) {
+    for (let i = 0; i < dots.length + 1; i++) {
+      if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+        currentSlide(i);
       }
-    }); */
+    }
+  });
 }
 
 module.exports = slider;
